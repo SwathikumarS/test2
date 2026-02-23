2c3ec1cab6412a8800bbb6c1a4878a8918f77744
SwathikumarS <itsmekumar2020@gmail.com>
2026-02-23 19:18:26 +0530

    Add files via upload

---
+++ supabase-config.js
@@ -0,0 +1,5 @@
+const supabaseUrl = 'https://rmsyscqplyuhzngvpwbh.supabase.co';
+const supabaseKey = 'sb_publishable_Jh4EosELVE7CmT9kws2yUw_SzuO68gA';
+
+// Initialize Supabase Client
+const _supabase = supabase.createClient(supabaseUrl, supabaseKey);
